b61610b862a5ecff7b29922ff788a41a447091aa
Matteo D.V. <matteo.divita@outlook.be>
2024-08-22 00:15:09 +0200

    [angular:pokeapi] feature: création du constructor et init de la card

--- angular/pokeapi/src/app/components/pokemon-list/pokemon-list.component.ts
+++ angular/pokeapi/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PokemonCardComponent } from "../pokemon-card/pokemon-card.component";
+import { Pokemon } from '../../models/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -9,5 +10,22 @@ import { PokemonCardComponent } from "../pokemon-card/pokemon-card.component";
   styleUrl: './pokemon-list.component.css'
 })
 export class PokemonListComponent {
+
+  pokemon1!: Pokemon;
+
+  constructor() {
+    this.pokemon1 = new Pokemon();
+    this.pokemon1.name = "Pikachu";
+    this.pokemon1.hp = 100;
+    this.pokemon1.type = "https://tinyurl.com/25sjakvj";
+    this.pokemon1.attackName = "Poudre";
+    this.pokemon1.attackDescription = "Cette attaque fait 10 dégâts à 1 des Pokémon de l'adversaire. Ce Pokémon ne s'applique pas aux dégâts faibles et résistants.";
+    this.pokemon1.weakness = "https://tinyurl.com/25sjakvj";
+    this.pokemon1.resistance = "https://tinyurl.com/25sjakvj";
+    this.pokemon1.attackStrenght = 10;
+    this.pokemon1.id = 1;
+    this.pokemon1.height = 50;
+    this.pokemon1.weight = 10;
+  }
 
 }
