19f33969ebd6b92a25888d67c176de844cbcbe57
Matteo D.V. <matteo.divita@outlook.be>
2024-08-28 14:43:18 +0200

    [python]:(base recap): 01. Variables

---
+++ python/recap_general/python_base/demo01-variables/variables.py
@@ -0,0 +1,32 @@
+# Déclaration + affectation
+x = 5
+print(x) # Affiche: 5
+
+
+# Déclaration de plusieurs variables en une seule ligne
+a, b, c = 1, 2, 3
+print(a, b, c) # Affiche: 1 2 3
+
+# int
+age = 28
+print(age) # 28
+print(type(age)) # Affiche: <class "int">
+
+# float
+taille = 180.2
+print(taille) 
+print(type(taille)) # Affichage: <class "float">
+
+# str
+message = "Bonjour tout le monde !"
+print(type(message)) # Affichage: <class "str">
+
+# bool
+is_active = True
+print(type(is_active)) # Affichage: <class "bool">
+
+
+# Conventions d'écriture
+
+nombre_de_livres = 5
+utilisateur_connecte = False
